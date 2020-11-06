ac1a052cd0fe2c15b7de97aa05fda3700f6e0478
kimn-dti <kimn@teknologisk.dk>
2020-11-06 09:42:46 +0100

    Added ENV's in dockerfile

--- scripts/robot_test.py
+++ scripts/robot_test.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3.5
 
 from cfrankr import Affine, Gripper, MotionData, Robot, Waypoint  # pylint: disable=E0611
+from multiprocessing import Process
+from subprocess import Popen
+import sys
+import time
+from typing import List, Optional
+
 
 gripper = Gripper('10.224.60.160', 0.06, 40.0)
 robot = Robot('panda_arm', 0.32)
 
-gripper.move(0.08)
+gripper.homing()
+#print()
